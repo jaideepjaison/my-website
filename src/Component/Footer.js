4eb2f47d4a7f954c6e31071814f4e8256133caa0
jaideepjaison <jaison3666@gmail.com>
2024-07-05 12:00:23 +0530

    udemy added icon

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -8,13 +8,9 @@ const Footer = () => {
     return ( 
         <>
 
-
-
-
-
 <body className='footer-body'>
  
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
   <title>Footer</title>
   
 <footer>
@@ -28,6 +24,7 @@ const Footer = () => {
       <a href="https://www.youtube.com/@codewithjaison367" target='_blank'><i className="fab fa-youtube"></i></a>
       <a href="https://www.linkedin.com/in/jaison-jaideep-lobo/" target='_blank'><i className="fab fa-linkedin-in"></i></a>
       <a href="https://github.com/jaideepjaison" target='_blank'><i className="fab fa-github"></i></a> 
+      <a href="https://www.udemy.com/user/jaison-jaideep-lobo/" target='_blank'><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M12 0L5.81 3.573v3.574l6.189-3.574l6.191 3.574V3.573zM5.81 10.148v8.144c0 1.85.589 3.243 1.741 4.234S10.177 24 11.973 24s3.269-.482 4.448-1.474c1.179-.991 1.768-2.439 1.768-4.314v-8.064h-3.242v7.85c0 2.036-1.509 3.055-2.948 3.055c-1.428 0-2.947-.991-2.947-3.027v-7.878z"/></svg></a> 
   </div>
        
   </div>
