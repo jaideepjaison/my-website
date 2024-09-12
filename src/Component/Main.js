57bbf47e9890c028709541e4a58d9b9e4854f004
jaideepjaison <jaison3666@gmail.com>
2024-09-12 12:19:30 +0530

    name changed

--- src/Component/Main.js
+++ src/Component/Main.js
@@ -5,7 +5,7 @@ const Main = () => {
     <div className="background-image"></div>
     <div className="content">
       <img src="PhotoJL.png" alt="Profile Photo" className="profile-photo" />
-      <h1 className="name">Jaison Lobo<span className="cursor"></span></h1>
+      <h1 className="name">Jaison J Lobo<span className="cursor"></span></h1>
       <h6 className="desig">Software Engineer</h6>
       <p className="description">I am a passionate software engineer with experience in Full Stack web development and Freelancing...</p>
     </div>
