728392c5a5f8fcce3fb38b623c85ede46c722a60
jaideepjaison <jaison3666@gmail.com>
2026-02-07 22:49:11 +0530

    Updates experience to 4 years in profile description
    Reflects current professional experience in the profile section
    Improves accuracy and credibility of displayed information

--- src/Component/Main.js
+++ src/Component/Main.js
@@ -8,7 +8,7 @@ const Main = () => {
       <img src={jllogo} alt="Profile Photo" className="profile-photo" />
       <h1 className="name">Jaison Lobo<span className="cursor"></span></h1>
       <h6 className="desig">Software Engineer</h6>
-      <p className="description">I am a passionate software engineer with experience in Full Stack web development and Freelancing...</p>
+      <p className="description">I am a passionate software engineer with 4 years of experience in Full Stack web development and Freelancing...</p>
     </div>
   </div>
     </> );
