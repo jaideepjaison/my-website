be4de539f67d0b245ed35a8af3bf63092a3807ca
jaideepjaison <jaison3666@gmail.com>
2026-02-07 22:11:37 +0530

    Adds new Best Buy project and updates project order
    Introduces a new project entry for Best Buy, updates the timeline and ordering of existing projects, and adjusts project numbering for clarity. Improves project history accuracy and presentation.

--- src/Component/Project/Project.js
+++ src/Component/Project/Project.js
@@ -2,6 +2,7 @@ import './Project.css';
 import ND from './ND_image.png';
 import st_roque from './st_logo.png';
 import aiet from './Aiet-College.jpg';
+import best_buy from './Best_Buy.jpg';
 const Project = () => {
     return ( 
         <>
@@ -10,19 +11,24 @@ const Project = () => {
     </header>
     <section className="projects">
       <div class="project">
-        <h3>Project 1 - July 2022- Oct 2024</h3>
+        <h3>Project 1 - Nov 2024 - Present</h3>
+        <a className='a-link' href="https://www.bestbuy.com/">Best Buy - Worksite App</a>
+        <img src={best_buy} alt="Project 1 Image" />
+      </div>
+      <div class="project">
+        <h3>Project 2 - July 2022- Oct 2024</h3>
         <a className='a-link' href="https://mmis.nd.gov/portals/wps/portal/EnterpriseHome">ND MMIS</a>
         <img src={ND} alt="Project 1 Image" />
       </div>
 
       <div className="project">
-        <h3>Project 2 - Dec 2023 - March 2024</h3>
+        <h3>Project 3 - Dec 2023 - March 2024</h3>
         <a href="https://www.neerudechurch.in">Neerude Church</a>
         <img className='a-link' src={st_roque} alt="Project 2 Image" />
       </div>
 
       <div className="project">
-        <h3>Project 3 - Dec 2021 - June 2022</h3>
+        <h3>Project 4 - Dec 2021 - June 2022</h3>
         <a href="https://saw-erp.in">AIET College ERP</a>
         <img className='a-link' src={aiet} alt="Project  Image" />
       </div>
